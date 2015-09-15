dfce57fb1f8c46eb289a0b0b616058a47287b2ba
Stuart Mumford <stuart.mumford@sheffield.ac.uk>
2015-09-15 16:51:55 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+import os
+from setuptools import setup
+
+setup(
+    name = "sgespawner",
+    license = "BSD",
+    packages=['sgespawner'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: BSD License",
+    ],
+)
